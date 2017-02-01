bef3659af89f15c1c5c0fabad26c201fd2ee6f41
aslmbc13 <asl_mbc_13@hotmail.com>
2017-02-01 23:59:57 +0000

    ...

---
+++ src/Algorithm.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+
+public class Algorithm {
+	
+	public static Vector<Slice> solve(int minIngredients, int maxCells, Pizza.Ingredients[][] matrix) {
+		return null;
+	}
+
+}
