8af06a433a448a7488d3ad532e636acf2c126e67
LuisMDuarte95 <luis.azevedoduarte@gmail.com>
2017-02-02 00:38:06 +0000

    Write output added

---
+++ src/Output.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+public class Output {
+	
+	public void write(Vector<Slice> slices, String filename) throws IOException{
+		
+		try{
+		    PrintWriter writer = new PrintWriter(filename, "UTF-8");
+		    writer.println(slices.size());
+		    for(int i=0; i<slices.size(); ++i){
+		    	writeSlice(slices.get(i), writer);
+		    }
+		    writer.close();
+		} catch (IOException e) {
+		   System.out.println("Error writing in file:" + filename);
+		}
+	}
+	
+	public void writeSlice (Slice s, PrintWriter write){
+		
+		write.println(s.x + " " + s.y + " " + (s.x + s.width) + " " + (s.y + s.height));
+	}
+}
