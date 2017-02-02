487b204845c6c06885e3facd6b2eaaa6a3913f1b
Gustavo Silva <silva95gustavo@gmail.com>
2017-02-02 00:08:43 +0000

    ...

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -6,4 +6,21 @@ public class Algorithm {
 		return null;
 	}
 
+	/**
+	 *
+	 * @param minIngredients
+	 * @param maxCells
+	 * @param matrix
+	 * @param x
+	 * @param y
+	 * @return the slice or null if impossible
+	 */
+	private static Slice findMaxSlice(int minIngredients, int maxCells, Pizza.Ingredients[][] matrix, int x, int y) {
+		// TODO
+		return null;
+	}
+
+	private static boolean isValidSlice(int minIngredients, int maxCells, Pizza.Ingredients[][] matrix, Slice slice) {
+		return true;
+	}
 }
