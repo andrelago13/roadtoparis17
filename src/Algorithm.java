d4061e70a4b29963dfbf7233a9963eca28aae948
aslmbc13 <asl_mbc_13@hotmail.com>
2017-02-02 00:14:23 +0000

    Solve

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -3,7 +3,23 @@ import java.util.Vector;
 public class Algorithm {
 	
 	public static Vector<Slice> solve(int minIngredients, int maxCells, Pizza.Ingredients[][] matrix) {
-		return null;
+		
+		int sizeY = matrix.length;
+		int sizeX = matrix[0].length;
+		
+		Vector<Slice> slices = new Vector<>();
+		
+		for(int x = 0; x < sizeX; ++x) {
+			for(int y = 0; y < sizeY; ++y) {
+				if(matrix[y][x] == Pizza.Ingredients.NONE) {
+					break;
+				}
+				
+				
+			}
+		}
+		
+		return slices;
 	}
 
 }
