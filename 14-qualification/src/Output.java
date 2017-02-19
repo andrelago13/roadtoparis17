d38c8a1280eebdaa339ed56de9cf0887c553c83f
LuisMDuarte95 <luis.azevedoduarte@gmail.com>
2017-02-19 23:44:15 +0000

    Output function added

--- 14-qualification/src/Output.java
+++ 14-qualification/src/Output.java
@@ -1,4 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
 
 public class Output {
+	
+	public static void write(Vector<Server> servers, String filename) throws IOException{
+		
+		try{
+		    PrintWriter writer = new PrintWriter(filename, "UTF-8");
+		    writer.println(servers.size());
+		    for(int i=0; i<servers.size(); ++i){
+		    	writeServer(servers.get(i), writer);
+		    }
+		    writer.close();
+		} catch (IOException e) {
+		   System.out.println("Error writing in file:" + filename);
+		}
+	}
+	
+	public static void writeServer (Server s, PrintWriter write){
 
+		write.println(s.posY + " " + s.posX + " " + s.pool);
+	}
 }
