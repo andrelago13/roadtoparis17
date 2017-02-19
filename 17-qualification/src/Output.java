fb1da103f7fb9c2bc3d16d17b0cd7a50aaeae462
aslmbc13 <asl_mbc_13@hotmail.com>
2017-02-19 22:58:00 +0000

    reorganize

---
+++ 17-qualification/src/Output.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+public class Output {
+	
+	public static void write(Vector<Slice> slices, String filename) throws IOException{
+		
+		try{
+		    PrintWriter writer = new PrintWriter(filename, "UTF-8");
+		    writer.println(slices.size());
+		    for(int i=0; i<slices.size(); ++i){
+		    	writeSlice(slices.get(i), writer);
+		    }
+		    writer.close();
+		} catch (IOException e) {
+		   System.out.println("Error writing in file:" + filename);
+		}
+	}
+	
+	public static void writeSlice (Slice s, PrintWriter write){
+
+		write.println(s.y + " " + s.x + " " + (s.y + s.height - 1) + " " + (s.x + s.width - 1));
+	}
+}
