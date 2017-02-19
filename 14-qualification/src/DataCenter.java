80aa8aa405c2a1ab0132ce3e58f101268f875721
LuisMDuarte95 <luis.azevedoduarte@gmail.com>
2017-02-19 23:55:44 +0000

    Server comparison function added

--- 14-qualification/src/DataCenter.java
+++ 14-qualification/src/DataCenter.java
@@ -1,6 +1,18 @@
+import java.util.Comparator;
 import java.util.Vector;
 
-public class DataCenter {
+public class DataCenter implements Comparator<Server>{
 	public int grid[][];
 	public Vector<Server> servers;
+	
+	@Override
+	public int compare(Server server1, Server server2) {
+		if(server1.capacity/server1.size > server2.capacity/server2.size){
+			return -1;
+		}
+		else if(server1.capacity/server1.size > server2.capacity/server2.size){
+			return 1;
+		}
+		return 0;
+	}
 }
