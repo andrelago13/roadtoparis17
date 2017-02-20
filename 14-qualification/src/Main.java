a5bb19b8649fbeacae835969ea723a0f6d7064be
LeonardoPCAlternativo <leonardosfpt@gmail.com>
2017-02-20 00:25:04 +0000

    read continue

--- 14-qualification/src/Main.java
+++ 14-qualification/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	static int unvSlots;
 	static int pools;
 	static int serversNum;
-	static int[][] servers;
+	static DataCenter data;
 	
 	public static void readFile(String filename) throws IOException {
 
@@ -23,14 +23,35 @@ public class Main {
 		pools = Integer.parseInt(details[3]);
 		serversNum = Integer.parseInt(details[4]);
 		
-		servers = new int [serversNum+unvSlots][2];
+		data.grid = new int[serversNum+unvSlots][2];
 		
 		String line = null;
-		int tempInd = 0;
+		int tempUnvSlots = 0;
+		int serverId = 0;
 		
-		while ((line = reader.readLine()) != null) {			
+		while ((line = reader.readLine()) != null) {
+			
+			if(tempUnvSlots <= unvSlots)
+			{
+				String infoLine = reader.readLine();
+				String[] detailsLine = infoLine.split(" ");
+				data.grid[Integer.parseInt(detailsLine[0])][Integer.parseInt(detailsLine[1])] = -1;
+			}
+			else
+			{
+				String infoLine = reader.readLine();
+				String[] detailsLine = infoLine.split(" ");
 				
-			tempInd++;
+				Server server = new Server();
+				server.id = serverId;
+				server.size = Integer.parseInt(detailsLine[0]);
+				server.capacity = Integer.parseInt(detailsLine[1]);
+				
+				data.servers.add(server);
+				serverId++;
+			}
+				
+			tempUnvSlots++;
 		}
 	}
 
