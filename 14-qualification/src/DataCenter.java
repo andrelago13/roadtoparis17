6592b34549278384b0ac8ec835cd5318487d5bf7
LuisMDuarte95 <luis.azevedoduarte@gmail.com>
2017-02-20 00:02:55 +0000

    Server comparators update

--- 14-qualification/src/DataCenter.java
+++ 14-qualification/src/DataCenter.java
@@ -1,18 +1,31 @@
 import java.util.Comparator;
 import java.util.Vector;
 
-public class DataCenter implements Comparator<Server>{
+public class DataCenter{
 	public int grid[][];
 	public Vector<Server> servers;
 	
-	@Override
-	public int compare(Server server1, Server server2) {
-		if(server1.capacity/server1.size > server2.capacity/server2.size){
-			return -1;
+	public static Comparator<Server> RatioComparator = new Comparator<Server>(){
+		public int compare(Server server1, Server server2){
+			if(server1.capacity/server1.size > server2.capacity/server2.size){
+				return -1;
+			}
+			else if(server1.capacity/server1.size > server2.capacity/server2.size){
+				return 1;
+			}
+			return 0;
 		}
-		else if(server1.capacity/server1.size > server2.capacity/server2.size){
-			return 1;
+	};
+	
+	public static Comparator<Server> IdComparator = new Comparator<Server>(){
+		public int compare(Server server1, Server server2){
+			if(server1.id > server2.id){
+				return -1;
+			}
+			else if(server1.id > server2.id){
+				return 1;
+			}
+			return 0;
 		}
-		return 0;
-	}
+	};
 }
