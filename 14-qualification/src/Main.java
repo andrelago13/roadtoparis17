8a9f3d202b8d8d98cc030da1d555db07c1e04752
LeonardoPCAlternativo <leonardosfpt@gmail.com>
2017-02-20 00:05:00 +0000

    Read started

--- 14-qualification/src/Main.java
+++ 14-qualification/src/Main.java
@@ -1,9 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+	
+	static int rows;
+	static int slotsPerRow;
+	static int unvSlots;
+	static int pools;
+	static int serversNum;
+	static int[][] servers;
+	
+	public static void readFile(String filename) throws IOException {
+
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		
+		String info = reader.readLine();
+		String[] details = info.split(" ");
+		rows = Integer.parseInt(details[0]);
+		slotsPerRow = Integer.parseInt(details[1]);
+		unvSlots = Integer.parseInt(details[2]);
+		pools = Integer.parseInt(details[3]);
+		serversNum = Integer.parseInt(details[4]);
+		
+		servers = new int [serversNum+unvSlots][2];
+		
+		String line = null;
+		int tempInd = 0;
+		
+		while ((line = reader.readLine()) != null) {			
+				
+			tempInd++;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
