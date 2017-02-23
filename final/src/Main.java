191b3b6944cd5f18773a9b072b7e342cca253c00
LeonardoPCAlternativo <leonardosfpt@gmail.com>
2017-02-23 18:50:34 +0000

    input continued

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Main {
@@ -8,6 +11,19 @@ public class Main {
 	static int numCaches;
 	static int cacheCapac;
 	static ArrayList<Video> videos;
+	static ArrayList<Endpoint> endpoints;
+	
+	
+	public static void readFile(String filename) throws FileNotFoundException{
+		
+		videos = new ArrayList<Video>();
+		endpoints = new ArrayList<Endpoint>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
