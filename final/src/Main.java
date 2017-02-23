b8e5967e841644f5842b3ac93054013a42aface5
LeonardoPCAlternativo <leonardosfpt@gmail.com>
2017-02-23 18:38:33 +0000

    Input started

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,5 +1,11 @@
 
 public class Main {
+	
+	static int numVideos;
+	static int numEndpoints;
+	static int requestDescrip;
+	static int numCaches;
+	static int cacheCapac;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
