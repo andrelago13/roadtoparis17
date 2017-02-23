30f359d7b65d105ce040d4b8a97a4d3388bcee5a
LeonardoPCAlternativo <leonardosfpt@gmail.com>
2017-02-23 19:03:40 +0000

    Input continued

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
@@ -14,13 +15,31 @@ public class Main {
 	static ArrayList<Endpoint> endpoints;
 	
 	
-	public static void readFile(String filename) throws FileNotFoundException{
+	public static void readFile(String filename) throws IOException{
 		
 		videos = new ArrayList<Video>();
 		endpoints = new ArrayList<Endpoint>();
 		
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
 		
+		String info = reader.readLine();
+		String[] details = info.split(" ");
+		numVideos = Integer.parseInt(details[0]);
+		numEndpoints = Integer.parseInt(details[1]);
+		requestDescrip = Integer.parseInt(details[2]);
+		numCaches = Integer.parseInt(details[3]);
+		cacheCapac = Integer.parseInt(details[4]);
+		
+		String infoVideos = reader.readLine();
+		String[] detailsVid = infoVideos.split(" ");
+		int videoId = 0;
+		
+		for (int i = 0; i < detailsVid.length; i++){
+			Video video = new Video();
+			video.id = videoId;
+			video.size = Integer.parseInt(detailsVid[videoId]);
+			videoId++;
+		}
 		
 		
 	}
--- final/src/Video.java
+++ final/src/Video.java
@@ -1,4 +1,8 @@
+import java.util.List;
+
 public class Video {
+	public int id;
 	public int numHits;
 	public int size;
+	public List<Request> requests;
 }
