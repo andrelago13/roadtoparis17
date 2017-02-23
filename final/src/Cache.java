eddb14d810c1df6856bc8971d92ef4ccd124b870
aslmbc13 <asl_mbc_13@hotmail.com>
2017-02-23 19:10:48 +0000

    cenas

--- final/src/Cache.java
+++ final/src/Cache.java
@@ -8,23 +8,23 @@ public class Cache {
 	public int size;
 	public List<Video> favourites;
 	public Set<Video> assigned;
-	public static ArrayList<Connection> connections;
+	public ArrayList<Connection> connections;
 	
 	public Comparator<Video> localRequestComparator = new Comparator<Video>() {
 		@Override
 		public int compare(Video v1, Video v2) {
 			
-			double v1_OriginalLatency = 0;
-			double v2_OriginalLatency = 0;
-			double v1_NewLatency = 0;
-			double v2_NewLatency = 0;
+			double v1_OriginalLatency = localVideoLatencyTimesRequests(v1);
+			double v2_OriginalLatency = localVideoLatencyTimesRequests(v2);
+			double v1_NewLatency = localVideoCacheLatencyTimesRequests(v1);
+			double v2_NewLatency = localVideoCacheLatencyTimesRequests(v2);
 			
 			double v1_score = (v1_OriginalLatency/v1_NewLatency) * (size/v1.size);
 			double v2_score = (v2_OriginalLatency/v2_NewLatency) * (size/v2.size);
 			
-			if(v1_score < v2_score) {
+			if(v1_score > v2_score) {
 				return -1;
-			} else if (v1_score > v2_score) {
+			} else if (v1_score < v2_score) {
 				return 1;
 			}
 			
@@ -33,7 +33,7 @@ public class Cache {
 		
 	};
 	
-	public static double localVideoLatencyTimesRequests(Video video, int cacheID) {
+	public double localVideoLatencyTimesRequests(Video video) {
 		double sumLatencyTimesRequests = 0;
 		for(Connection c : connections) {
 			sumLatencyTimesRequests += c.endpoint.latency*c.endpoint.getVideoRequests(video);
@@ -41,7 +41,7 @@ public class Cache {
 		return sumLatencyTimesRequests;
 	}
 	
-	public static double localVideoCacheLatencyTimesRequests(Video video, int cacheID) {
+	public double localVideoCacheLatencyTimesRequests(Video video) {
 		double sumLatencyTimesRequests = 0;
 		for(Connection c : connections) {
 			sumLatencyTimesRequests += c.latency*c.endpoint.getVideoRequests(video);
